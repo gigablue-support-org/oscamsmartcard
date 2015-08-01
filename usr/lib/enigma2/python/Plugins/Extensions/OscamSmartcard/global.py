7536af02b0fa558a6ca46adff4560ee838652b16
undertaker01 <spichala@gmx.de>
2015-08-01 13:09:33 +0200

    add global file

---
+++ usr/lib/enigma2/python/Plugins/Extensions/OscamSmartcard/global.py
@@ -0,0 +1,39 @@
+message01=''
+message02='install or update to the latest version'
+message03=' '
+message04='Oscam binary install/update'
+message05='OscamSmartcard is now running'
+message06='online avaible'
+message07=''
+message08='Remove all first.\notherwise Oscamsmartcard will not start\nPress GREEN to remove all\nAll config files are backed up automatically'
+message09=''
+message10=''
+message11='file exists, but is not executable'
+message12='Uninstall all Softcams ?'
+message13='Warning'
+message14='unused Softams,Config or others found'
+message15='Error'
+message16='All Softcams will be deinstalled\nAre you sure ?'
+message17='found installed Softcams to be remove' 
+message18='no other softcams found'
+message19='no oscam_oscamsmartcard found'
+message20='Ok Done\nOscamsmartcard is now running'
+message21='remove'
+
+
+message26='-------------------------------------- Auto Config----------------------------------------'
+
+
+#ignore plugin's by makeclean
+ignore1='enigma2-plugin-softcams-oscamsmartcard'
+ignore2='enigma2-plugin-pli-softcamsetup'
+ignore3='enigma2-plugin-softcams-oscamstatus'
+
+
+#system
+plugin='[OscamSmartcard] '
+null =' >/dev/null 2>&1'
+opkginstallcheck= 'opkg list-installed |grep -i softcam'
+deinstall ='opkg remove --force-remove '
+
+
